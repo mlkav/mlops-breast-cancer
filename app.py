0791d9aead710747e3b6c9cc8468d7a2e5c0c40a
maulanakavaldo <alkav.maulana@gmail.com>
2024-09-06 15:02:06 +0700

    update

--- app.py
+++ app.py
@@ -71,7 +71,10 @@ def index():
     
     return render_template('index.html', diagnosis=None)
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+# if __name__ == "__main__":
+#     import os
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port, debug=False)
+
+if __name__ == '__main__':
+  app.run(port=5000)
